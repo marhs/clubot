5ce126f3406afde7df68e470ebda675ad36e3ad0
Marco Herrero <me@marhs.de>
2014-01-15 11:15:46 +0100

    Initial commit. Added imgur.py and config files.

---
+++ config-example.py
@@ -0,0 +1,19 @@
+### Configuration file for Clubot ###
+# 
+# Rename this file to config.py and run clubot.py
+
+# Imgur API key
+imgurClientId   = 'imgurAPIKEY'
+
+# Working dirs. Clubot will upload imgs from imgDir and move that images to
+# completedDir. 
+imgDir          = 'img/'
+completedDir    = 'done/'
+
+# Reddit username/password
+# Be careful, DO NOT COMMIT THIS WITH REAL USER/PASS
+user            = 'username'
+password        = 'password'
+
+# Subreddit name to publish
+subreddit       = 'gggggggg'
